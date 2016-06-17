65965c530498cc4fd08f2ee3f1b8d96d7b5de023
Sander Toonen <sander@ib.nl>
2016-06-17 20:22:41 +0200

    Add project code

---
+++ src/MongoDbServiceProvider.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Xatoo\Silex\ServiceProvider;
+
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+
+/**
+ * MongoDb service provider
+ *
+ * @author Sander Toonen <s.toonen@gmail.com>
+ */
+class MongoDbServiceProvider implements ServiceProviderInterface
+{
+    const MONGODB = 'mongodb';
+    const MONGODB_CONNECTIONS = 'mongodb.connections';
+    const MONGODB_FACTORY = 'mongodb.factory';
+
+    /**
+     * Registers services on the given container.
+     *
+     * @param Container $container
+     */
+    public function register(Container $container)
+    {
+        // Configured connections
+        $container[self::MONGODB_CONNECTIONS] = [
+            'default' => [
+                'server' => 'mongodb://localhost:27017',
+                'options' => ['connect' => true],
+            ],
+        ];
+
+        // Factory for creating new connections
+        $container[self::MONGODB_FACTORY] = $container->protect(function ($server = 'mongodb://localhost:27017', array $options = ['connect' => true]) {
+            return new \MongoDB\Client($server, $options);
+        });
+
+        // All MongoDb connections
+        $container[self::MONGODB] = function ($container) {
+            $connections = new Container();
+            foreach ($container[MongoDbServiceProvider::MONGODB_CONNECTIONS] as $name => $options) {
+                $connections[$name] = function () use ($options, $container) {
+                    return $container[MongoDbServiceProvider::MONGODB_FACTORY]($options['server'], $options['options']);
+                };
+            }
+            return $connections;
+        };
+    }
+}
+
---
+++ tests/MongoDbServiceProviderTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Xatoo\Silex\tests;
+
+use Silex\Application;
+use Xatoo\Silex\ServiceProvider\MongoDbServiceProvider;
+
+class MongoDbServiceProviderTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Application
+     */
+    protected $app;
+
+    protected $mongoClass;
+
+    public function setUp()
+    {
+        $this->app = new Application();
+        $this->app->register(new MongoDbServiceProvider(), [
+            'mongodb.connections' => [
+                'default' => [
+                    'server' => 'mongodb://localhost:27017',
+                    'options' => ['connect' => false],
+                ],
+            ],
+        ]);
+        $this->mongoClass = '\MongoDB\Client';
+    }
+
+    public function testConnectionProvider()
+    {
+        $this->assertInstanceOf($this->mongoClass, $this->app['mongodb']['default']);
+    }
+
+    public function testFactory()
+    {
+        $factory = $this->app['mongodb.factory'];
+        $connection = $factory('mongodb://localhost:27017', ['connect' => false]);
+        $this->assertInstanceOf($this->mongoClass, $connection);
+    }
+
+    public function testConnection()
+    {
+        if (!extension_loaded('mongodb')) {
+            $this->markTestSkipped('mongodb extension is not installed');
+        }
+
+        $app = new Container();
+        $app->register(new MongoDbServiceProvider());
+        $mongodb = $app['mongodb']['default'];
+        $database = $mongodb->selectDatabase('xatoo-silex2-mongodb-provider');
+        $collection = $database->selectCollection('test');
+        $document = ['foo' => 'bar'];
+        $collection->insert($document);
+        $this->assertArrayHasKey('_id', $document);
+        $database->dropCollection('test');
+    }
+}
